18da7f24c2722d19f9bec9deab4ba6f1e6a3191b
sneshyba <nesh@pugetsound.edu>
2022-01-04 13:00:36 -0800

    Got rid of the global PchemLibrary

--- Thermo 2022 (Neshyba)/Week_10b.PathIndependence/PchemLibrary.py
+++ Thermo 2022 (Neshyba)/Week_10b.PathIndependence/PchemLibrary.py
@@ -1,4 +1,6 @@
 import numpy as np
+from scipy.interpolate import RectBivariateSpline
+import matplotlib.pyplot as plt
 
 def Statespace(xspecs,yspecs):
     xarray = np.linspace(xspecs[0],xspecs[1],xspecs[2])
@@ -7,6 +9,33 @@ def Statespace(xspecs,yspecs):
     xgrid = xgridtemp
     ygrid = ygridtemp
     return xgrid, ygrid
+
+def plot_surface(Xgrid, Ygrid, Zgrid, color='purple'):
+    # Creates a surface plot in the handle myax
+    
+    ax = plt.figure().gca(projection='3d') # Set up a three dimensional graphics window 
+
+    # This strips out units if necessary
+    if hasattr(Xgrid,'units'):
+        Xgridplot = Xgrid.magnitude
+    else:
+        Xgridplot = Xgrid
+        
+    if hasattr(Ygrid,'units'):
+        Ygridplot = Ygrid.magnitude
+    else:
+        Ygridplot = Ygrid
+        
+    if hasattr(Zgrid,'units'):
+        Zgridplot = Zgrid.magnitude
+    else:
+        Zgridplot = Zgrid
+        
+    # Now plot
+    ax.plot_surface(Xgridplot, Ygridplot, Zgridplot, color=color)
+    
+    # Now return the handle
+    return ax
 
 def dF_dx(statespace,Fgrid):
     # Returns the partial of F with respect to x (axis 0) holding y (axis 1) constant
@@ -17,8 +46,8 @@ def dF_dx(statespace,Fgrid):
     dF_dx = dF/dx
     print('Shape of partial derivative =', np.shape(dF_dx))
     try:
+        dF_dx *= Fgrid.units/xgrid.units
         print('Units of partial derivative =', dF_dx.units)
-#         dF_dx *= Fgrid.units/xgrid.units
     except:
         print('No units')
     xgridnew = xgrid[1:,:]
@@ -34,13 +63,31 @@ def dF_dy(statespace,Fgrid):
     dF_dy = dF/dy
     print('Shape of partial derivative =', np.shape(dF_dy))
     try:
+        dF_dy *= Fgrid.units/ygrid.units
         print('Units of partial derivative =', dF_dy.units)
-#         dF_dy *= Fgrid.units/ygrid.units
     except:
         print('No units')
     xgridnew = xgrid[:,1:]
     ygridnew = ygrid[:,1:]
     return xgridnew, ygridnew, dF_dy
+
+def func_P_isotherm(V1,V2,n,R,T,AssignQuantity,P_units):
+    # Defines an isothermal expansion/contraction function
+    Varray = np.linspace(V1,V2)
+    Varray = AssignQuantity(Varray,V1.units)
+    Parray = n*R*T/Varray
+    Parray.ito(P_units)
+    return Varray, Parray
+
+def func_P_adiabat(V1,V2,n,R,T1,C_V,AssignQuantity,P_units):
+    # Defines an adiabatic expansion/contraction function
+    V2array = np.linspace(V1,V2)
+    V2array = AssignQuantity(V2array,V2.units)
+    P1 = n*R*T1/V1
+    nR_over_C_V = n*R/C_V
+    P2array = P1*(V2array/V1)**(-nR_over_C_V-1)
+    P2array.ito(P_units)
+    return V2array, P2array
 
 def CP_H2Ogas(T,AssignQuantity):
     """ www.engineeringtoolbox.com/water-vapor-d_979.html """
@@ -56,7 +103,7 @@ def CP_H2Oice(T,AssignQuantity):
     return CP
 
 def CP_H2Oliq(T,AssignQuantity):
-    """ webbook.nist.gov """
+    """ https://webbook.nist.gov/cgi/cbook.cgi?ID=C7732185&Units=SI&Mask=2#Thermo-Condensed """
     A = AssignQuantity(-203.606,'J/mol/K')
     B = AssignQuantity(1523.290,'J/mol/K^2')
     C = AssignQuantity(-3196.413,'J/mol/K^3')
@@ -65,6 +112,8 @@ def CP_H2Oliq(T,AssignQuantity):
     t = T/1000
     CP = A + B*t + C*t**2 + D*t**3 + E/t**2
     return CP
+
+
 
 def Integrator(statespace,dF_dx,dF_dy,AssignQuantity,SState=[],Units=[],axis=0):
     """Integrates a differential equation of state to produce F(x,y)"""
@@ -111,4 +160,23 @@ def Integrator(statespace,dF_dx,dF_dy,AssignQuantity,SState=[],Units=[],axis=0):
         Fgrid += SState_F
 
     return(Fgrid)
-    
+   
+def StateSpaceInterpolator(statespace,nxarray,nyarray,Fgrid,AssignQuantity=0):
+    if type(AssignQuantity) == type:
+        #print('I think it is a function')
+        useAssignQuantity = True
+    else:
+        useAssignQuantity = False
+    xgrid = statespace[0]
+    ygrid = statespace[1]
+    Fgrid_interpolater = RectBivariateSpline(xgrid[:,0], ygrid[0,:], Fgrid)
+    if np.size(nxarray) == 1:
+        nxarray = [nxarray]
+        nyarray = [nyarray]
+    result = []
+    for i in range(len(nxarray)):
+        result.append(Fgrid_interpolater(nxarray[i],nyarray[i]))
+    result = np.squeeze(result)
+    if useAssignQuantity:
+        result = AssignQuantity(result,Fgrid.units)
+    return np.squeeze(result)
